e43734aed11557b45d9f2ce8d6e6dae2a1ec6e1a
Maysa Orazova <[maysaorash@gmail.com]>
2020-11-23 12:03:50 -0500

    added promises1

---
+++ index.js
@@ -0,0 +1,14 @@
+// let orderPromise = require('./1-Promise-Object.js')
+require('./2-setTimeout-Function.js')
+// let timeout = require('./2-setTimeout-Function.js')
+
+// let {atamuradAnswer, handleSuccess, handleFailure} = require('./3-onFulfilled-and-onRejected-Functions.js');
+// require('./4-catch-Function.js');
+// require('./5-Chaining-Multiple-Promises.js')
+//  require('./6-Common-Mistakes.js')
+// require('./7-promiseAll-Funciton.js')
+// console.log(orderPromise)
+
+// atamuradAnswer.then(handleSuccess).then(null, handleFailure)
+// atamuradAnswer.then(handleSuccess).catch(handleFailure)
+
